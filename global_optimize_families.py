d41c8b93253fa1e52ccb3492071c3a6b8a652363
Ben Evans <benjamin.robert.evans@gmail.com>
2013-12-07 16:06:20 -0500

    Slow, but it works!
    fixed union_len to actually return the length of the union of the loci
    of all the crosses passed in

--- global_optimize_families.py
+++ global_optimize_families.py
@@ -2,9 +2,7 @@
 import re
 import sys
 from collections import defaultdict
-import matplotlib.pyplot as plt
 from itertools import combinations
-from matplotlib_venn import venn3_unweighted 
 
 def check_locus(m, f):
     '''
@@ -50,10 +48,15 @@ def read_genotypes(file_name, pairs):
                         informative[pair].add(locus_name)
     return informative, loci
 
-def union_len(things):
+def union_len(pair_combo, informative):
+    '''
+    returns the length of the union of the informative loci of 
+    all the crosses passed in
+    '''
+
     uni = set()
-    for x in things:
-        uni.unioon(x)
+    for p in pair_combo:
+        uni.update(informative[p])
     return len(uni)
 
 
@@ -65,12 +68,12 @@ def global_optimize_crosses(max_k, pairs, informative):
     best_sets = []
     for k in range(2,max_k+1):
         best_num_loci = 0
-        for st in  combinations(pairs, k):
-            current_num_loci = union_len(st)
+        for pair_combo in combinations(pairs, k):
+            current_num_loci = union_len(pair_combo, informative)
             if best_num_loci < current_num_loci:
-                best_set = st
+                best_combo = pair_combo
                 best_num_loci = current_num_loci
-        best_sets.append(best_set)
+        best_sets.append(best_combo)
 
     return best_sets
 
@@ -89,26 +92,7 @@ if __name__ == '__main__':
 
     #get the loci captured at each k, the number of added loci,
     #and the pairs decending order of how many more loci they add
-    best_sets = global_optimize_crosses(6, pairs, informative)
+    best_combos = global_optimize_crosses(6, pairs, informative)
 
-    #labels for pairs
-    best_pairs_labels=["{0} x {1}".format(*x) for x in best_sets]
-    '''
-    #plot results
-    fig = plt.figure(figsize = (8,8))
-    ax1 = plt.subplot2grid((3,3), (0,0), colspan = 3, rowspan = 2)
-    ax1.set_title('Shared Loci in Top Three Pairs', fontsize = 26)
-    #venn diagram on top
-    venn3_unweighted([informative[x] for x in best_pairs[:3]], 
-                     set_labels = best_pairs_labels[:3], ax=ax1)
-    #bar chart on bottom
-    ax2 = plt.subplot2grid((3,3), (2,0), colspan=3)
-    ax2.bar(range(len(added_loci[:8])), added_loci[:8])
-    ax2.set_ylabel('Number of Loci')
-    ax2.set_title('Added Informative Loci Per Pair', fontsize = 26)
-    ax2.set_xticklabels(best_pairs_labels[:8], rotation = 17 )
-    #add a bit of extra space
-    fig.subplots_adjust(hspace=.44)
-    fig.savefig('summmary.pdf')
-    plt.show()
-    '''
+    for combo in best_combos:
+        print(combo, ":", union_len(combo, informative))
